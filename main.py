595bf0c5cd0ce58f1a8594c1ce1397955c963f9e
pragadajayasree <134396518+pragadajayasree@users.noreply.github.com>
2024-05-21 20:14:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -24,42 +25,39 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-
-#Write your code below this line
-import random
-p=int(input("enter 0-rock,1-paper,2-scissor"))
-q=random.randint(0,2)
-if(p==0):
- print(rock)
-elif(p==1):
- print(paper)
+p = int(input("enter 0-rock,1-paper,2-scissor"))
+q = random.randint(0, 2)
+if p == 0:
+    print(rock)
+elif p == 1:
+    print(paper)
 else:
-  print(scissors)
+    print(scissors)
 print("computers choice")
-if(q==0):
- print(rock)
-elif(q==1):
- print(paper)
+if q == 0:
+    print(rock)
+elif q == 1:
+    print(paper)
 else:
-  print(scissors)
-if(p==0):
-  if(q==0):
-    print("tie");
-  elif(q==1):
-    print("you lose")
-  else:
-    print("you win")
-elif(p==1):
-  if(q==0):
-    print("you win")
-  elif(q==1):
-    print('tie')
-  else:
-    print("you lose")
+    print(scissors)
+if p == 0:
+    if q == 0:
+        print("tie")
+    elif q == 1:
+        print("you lose")
+    else:
+        print("you win")
+elif p == 1:
+    if q == 0:
+        print("you win")
+    elif q == 1:
+        print('tie')
+    else:
+        print("you lose")
 else:
-  if(q==0):
-    print("you lose")
-  elif(q==1):
-    print("you win")
-  else:
-    print("tie")
+    if q == 0:
+        print("you lose")
+    elif q == 1:
+        print("you win")
+    else:
+        print("tie")
